df6f8a094601b66f9d8eef66a1bca5ecbd1335b9
Godfrey Lebo <emorylebo@gmail.com>
2023-09-15 20:23:56 +0100

    UPDATE

--- .history/src/pages/index_20230915201758.tsx
+++ .history/src/pages/index_20230915201758.tsx
@@ -9,25 +9,26 @@ const HomePage: React.FC = () => {
    
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">Fixpoint project</p>
+          <p className="text-base font-semibold text-indigo-600">Fixpoint</p>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Site and App currently under construction</h1>
-          <img
-            src="/logo.jpg"
-            alt=""
-            className="h-20 w-20"
-          />
-          <p className="mt-6 text-base leading-7 text-gray-900">Launch in</p>
           
           <Countdown targetDate={targetDate} />
-         
+          <img
+            src=""
+            alt=""
+            className="absolute inset-0 h-full w-full object-cover"
+          />
+          <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="#"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Download
+              Go back home
             </a>
-            
+            <a href="#" className="text-sm font-semibold text-gray-900">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </a>
           </div>
         </div>
       </main>
